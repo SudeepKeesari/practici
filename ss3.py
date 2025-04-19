1eda06a0d1730e415d3049a6fc6b452d50efac8b
unknown <keesari30@gmail.com>
2025-04-19 17:16:40 +0530

    practice3

---
+++ ss3.py
@@ -0,0 +1,7 @@
+rows=int(input("enter no.of rows"))
+for num in range(rows+1):  
+        for i in range(num):  
+            print(num, end=" ")       
+  
+        #End line after each row  
+        print()
